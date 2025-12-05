53c9810519fb1922fba5e34942854f74600021e7
KKarthick2004 <karthick.k2022cse@sece.ac.in>
2025-12-05 14:25:01 +0530

    Adding Third Program

---
+++ Program_3.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Main{
+	public static void main(String args[]){
+		Scanner scan=new Scanner(System.in);
+		int n=scan.nextInt();
+		
+		int arr[]=new int[n];
+		
+		for(int i=0;i<n;i++){
+			arr[i]=scan.nextInt();
+		}
+		
+		System.out.print(UserMainCode.findTriplet(arr));
+		
+	}
+}
+
+class UserMainCode{
+	   public static boolean findTriplet(int[] nums){
+	   	for(int i=0;i<nums.length-2;i++){
+	   		if(nums[i]==nums[i+1] && nums[i+1]==nums[i+2]){
+	   			return true;
+	   		}
+	   	}
+	   	return false;
+	   } 
+}
